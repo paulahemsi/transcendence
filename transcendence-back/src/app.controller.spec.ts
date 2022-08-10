f092fc26510c394734ea840ccca5d957477c7ebe
paulahemsi <paulahemsi@gmail.com>
2022-08-09 21:51:56 -0300

    poc(integration): add Abacatinho endpoint to test front-back integration

--- transcendence-back/src/app.controller.spec.ts
+++ transcendence-back/src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHelloAbacatinho()).toBe('Hello Abacatinho!\n');
     });
   });
 });
