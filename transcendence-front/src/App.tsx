727f1717c7e482fe8e0a455a1db3412421ff0c3f
paulahemsi <paulahemsi@gmail.com>
2022-08-09 21:54:20 -0300

    poc(integration): add axios lib to handle http request

--- transcendence-front/src/App.tsx
+++ transcendence-front/src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import avocado from './avocado.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+const handleClick = () => {
+  axios
+  .get("http://localhost:4444/Abacate")
+  .then(response => {
+    console.log(response.data);
+  });
+  }
+  
+  function App() {
   return (
     <div className="App">
       <header className="App-header">
