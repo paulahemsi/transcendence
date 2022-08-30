dc9e1037fe84bc8865815fc954c8e46ace20288e
paulahemsi <paulahemsi@gmail.com>
2022-08-29 22:38:06 -0300

    db(match-history): add match history

--- transcendence-back/src/users/users.module.ts
+++ transcendence-back/src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UserController, UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entity';
+import { MatchHistory } from 'src/entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, MatchHistory])],
   controllers: [UsersController, UserController],
   providers: [UsersService],
   exports: [UsersService],
