8677df24f98cc6bdf71b0a4797ff70254c63d218
paulahemsi <paulahemsi@gmail.com>
2022-09-01 13:56:21 -0300

    fix(import): remove unused import

--- transcendence-back/src/users/users.service.ts
+++ transcendence-back/src/users/users.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ResolveConfigOptions } from 'prettier';
 import { CreateUserDto, UpdateUserDto } from 'src/dto/users.dtos';
 import { User } from 'src/entity';
 import { MatchHistory } from 'src/entity';
