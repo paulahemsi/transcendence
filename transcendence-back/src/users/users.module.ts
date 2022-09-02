478a7e58f2193a5a83a448da6e857c93c4823c17
paulahemsi <paulahemsi@gmail.com>
2022-09-01 21:08:40 -0300

    chore(providers): add MatchHistoryService as user module provider

--- transcendence-back/src/users/users.module.ts
+++ transcendence-back/src/users/users.module.ts
@@ -4,11 +4,12 @@ import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entity';
 import { MatchHistory } from 'src/entity';
+import { MatchHistoryService } from 'src/match-history/match-history.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, MatchHistory])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, MatchHistoryService],
   exports: [UsersService],
 })
 export class UsersModule {}
