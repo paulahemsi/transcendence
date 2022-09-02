cb33622bb8d55bf806e0f9d7cd8d12a829ff182e
paulahemsi <paulahemsi@gmail.com>
2022-09-01 21:10:28 -0300

    refactor(math-history-service): split match history to his own service

--- transcendence-back/src/users/users.service.ts
+++ transcendence-back/src/users/users.service.ts
@@ -2,8 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto, UpdateUserDto } from 'src/dto/users.dtos';
 import { User } from 'src/entity';
-import { MatchHistory } from 'src/entity';
 import { Repository } from 'typeorm';
+import { MatchHistoryService} from 'src/match-history/match-history.service';
 
 export class matchInfos {
   opponent: string;
@@ -16,8 +16,8 @@ export class matchInfos {
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
-    @InjectRepository(MatchHistory) private readonly matchHistoryRepository: Repository<MatchHistory>,
-  ) {}
+    private matchHistoryService: MatchHistoryService,
+    ) {}
 
   getUsers() {
     return this.userRepository.find();
@@ -26,79 +26,7 @@ export class UsersService {
   findUser(id: string) {
     return this.userRepository.findOneBy({ id });
   }
-/****/
-  isPlayer1theWinner(match: MatchHistory) : boolean {
-    return match.player1Score > match.player2Score;
-  }
-  
-  isPlayer1(id: string, match: MatchHistory) : boolean {
-    return match.player1.id === id;
-  }
-  
-  setOpponentsName(isUserPlayer1: boolean, match: MatchHistory) : string {
-    if (isUserPlayer1) {
-      return match.player2.username;
-    }
-    return match.player1.username;
-  }
-  
-  setUserScore(isUserPlayer1: boolean, match: MatchHistory) : number {
-    if (isUserPlayer1) {
-      return (match.player1Score);
-    }
-    return match.player2Score;
-  }
-  
-  setOpponentScore(isUserPlayer1: boolean, match: MatchHistory) : number {
-    if (isUserPlayer1) {
-      return match.player2Score;
-    }
-    return match.player1Score;
-  }
-  
-  isUserTheWinner(matchInfo: matchInfos) : boolean {
-    return matchInfo.userScore > matchInfo.opponentScore;
-  }
-  
-  async executeMatchHistoryQuery(id: string) : Promise<MatchHistory[]> {
-    return await this.matchHistoryRepository.find({
-      select: {
-        player1Score: true,
-        player2Score: true,
-    },
-      relations: {
-        player1: true,
-        player2: true,
-    },
-      where: [
-        { player1: { id: id } },
-        { player2: { id: id } },
-      ]
-    });
-  }
-  
-  async buildMatchHistory(id: string) {
-    
-    const matches: Awaited<Promise<MatchHistory[]>> = await this.executeMatchHistoryQuery(id);
-    
-    let matchHistory: Array<matchInfos> = [];
-    
-    matches.map( (match) => {
-        let matchInfo : matchInfos = new matchInfos();
-        let isUserPlayer1 : boolean = this.isPlayer1(id, match);
 
-        matchInfo.opponent = this.setOpponentsName(isUserPlayer1, match);
-        matchInfo.userScore = this.setUserScore(isUserPlayer1, match);
-        matchInfo.opponentScore = this.setOpponentScore(isUserPlayer1, match);
-        matchInfo.isWinner = this.isUserTheWinner(matchInfo);
-
-        matchHistory.push(matchInfo);
-      }
-    )
-    
-    return matchHistory;
-  }
-  
   async getUserProfile(id: string) {
 
     const { username, rating, status } = await this.findUser(id);
@@ -107,7 +35,7 @@ export class UsersService {
       name: username,
       status: status,
       rating: rating,
-      matchHistory: await this.buildMatchHistory(id),
+      matchHistory: await this.matchHistoryService.buildMatchHistory(id),
     };
 
     return profile;
