93108a10f47d470becf578ecafde911de8b0ef97
lrcouto <laurarccouto@gmail.com>
2022-09-07 20:13:23 -0300

    Add CSS to login card component

--- transcendence-front/src/App.tsx
+++ transcendence-front/src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import Button from '@mui/material/Button';
-import avocado from './avocado.svg';
 import './App.css';
 import { useCallback, useState } from 'react';
-import { Box } from '@mui/material';
 import LoginCard from './components/LoginCard'
   
   function App() {
@@ -12,16 +9,11 @@ import LoginCard from './components/LoginCard'
       <head>
         <title>ft_transcendence</title>
         <meta name="description" content="ft_transcendence" />
-        <link rel="icon" href="/42.svg" />
+        <link rel="icon" href="/42.ico" />
       </head>
 
       <main>
-        <Box  display="flex" 
-              alignItems="center"
-              justifyContent="center"
-        >
           <LoginCard/>
-        </Box>
       </main>
     </>
   );
--- transcendence-front/src/components/LoginCard.tsx
+++ transcendence-front/src/components/LoginCard.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { Box, Button, Card, CardActions, CardContent, Typography } from '@mui/material';
-// import { makeStyles, createStyles } from '@mui/styles'; não tá rolando importar essa lib aqui 
-
-// const useStyles = makeStyles({
-// 	nomeDaClasse: {
-// 		//aqui vai a personalização CSS
-// 	},
-//   });
+import { Button, Card, CardActions, CardContent, Typography, Box } from '@mui/material';
 
 const LoginCardButton = () => {
 	return (
 		<>
 			<a href="http://localhost:4444/auth/login">
-				<Button variant="contained" size="large" sx={{ width: 150, height: 55, background: '#9575CD'}}>
+				<Button variant="contained" size="large"
+				sx={{ 
+					width: 150,
+					height: 55,
+					textTransform: 'lowercase',
+					background: '#9575CD',
+					fontFamily: 'Orbitron',
+					fontSize: 20}}>
 				login
 				</Button>
 			</a>
@@ -34,23 +34,34 @@ const LoginCardContent = () => {
 	return (
 		<>
 			<CardContent style={{ paddingLeft: 35, paddingTop: 25 }}>
-			<Typography align="left" sx={{ fontSize: 24}}>{'> transcend?'}</Typography>
+			<Typography align="left" sx={{ fontSize: 24, fontFamily: 'Orbitron', fontWeight: 500}}>{'> transcend?'}</Typography>
 			</CardContent>
 		</>
 	)
 }
 
-const LoginCard = () => {
-	//const classes = useStyles();
-
-	return (
-		<>
-			<Card /*className={classes.nomeDaClasse}*/ variant="outlined" sx={{ width: 500, height: 230, background: '#F5F5F5'}}>
-				<LoginCardContent />
-				<LoginCardActions />
-			</Card>
+export class LoginCard extends React.Component<{}, {}> {
+    render() {
+        return (
+            <>
+			<Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+				<Card variant="outlined" 
+					sx={{
+						width: 500,
+						height: 230,
+						background: '#F5F5F5',
+						border: 2,
+						borderColor: '#212121',
+						borderRadius: 3,
+						boxShadow: 5
+						}}>
+					<LoginCardContent />
+					<LoginCardActions />
+				</Card>
+			</Box>
 		</>
-	)
+        );
+    }
 }
 
 export default LoginCard
