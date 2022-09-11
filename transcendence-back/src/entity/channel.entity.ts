f3ded25ec7506f6e3fda2f2d53ade7b5d9990138
paulahemsi <paulahemsi@gmail.com>
2022-09-11 15:45:21 -0300

    feat(chat): add channel entity

---
+++ transcendence-back/src/entity/channel.entity.ts
@@ -0,0 +1,39 @@
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from './user.entity';
+import { ChannelType } from './channel-type.entity';
+import { Message } from './message.entity';
+
+
+@Entity()
+export class Channel {
+  @PrimaryGeneratedColumn({
+    type: 'bigint',
+  })
+  id: number;
+
+  @Column({
+    nullable: false,
+  })
+  name: string;
+
+  @ManyToOne(() => User, (user) => user.id, {
+    nullable: false,
+  })
+  //?Faz sentido type ser uma tabela a parte? pq não é um define?
+  type: ChannelType;
+  
+  @ManyToOne(() => User, (user) => user.id, {
+    nullable: false,
+  })
+  owner: User;
+
+  @Column({
+    nullable: true,
+  })
+  //*Nota: aqui lembrar de passar pelo algoritmo de hash 
+  password: string;
+
+  @OneToOne(() => Message)
+  @JoinColumn()
+  messages: Message;
+}
