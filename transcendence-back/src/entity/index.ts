ad74b63bd666a0144530d3c1f07266cf4197d8ca
paulahemsi <paulahemsi@gmail.com>
2022-09-11 16:59:39 -0300

    chore(entities): add new entity to index

--- transcendence-back/src/entity/index.ts
+++ transcendence-back/src/entity/index.ts
@@ -1,8 +1,11 @@
 import { User } from './user.entity';
 import { MatchHistory } from './match-history.entity';
 import { Friendship } from './friendship.entity';
+import { Channel } from './channel.entity';
+import { Message } from './message.entity';
+import { ChannelType } from './channel-type.entity';
 
-const entities = [User, MatchHistory, Friendship];
+const entities = [User, MatchHistory, Friendship, Channel, Message, ChannelType];
 
-export { User, MatchHistory, Friendship };
+export { User, MatchHistory, Friendship, Channel, Message, ChannelType };
 export default entities;
