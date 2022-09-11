62a6ea58b37e655df1a301f0365af572690d40e9
lrcouto <laurarccouto@gmail.com>
2022-09-11 02:49:19 -0300

    Add user profile info to app bar

--- transcendence-front/src/components/Home.tsx
+++ transcendence-front/src/components/Home.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Typography, Box, Button, AppBar } from '@mui/material';
+import { Typography, Box, Button, AppBar, Stack } from '@mui/material';
 import axios from 'axios';
 import jwt from 'jwt-decode';
+import { borderRadius } from "@mui/system";
 
 type booleanSetState = React.Dispatch<React.SetStateAction<boolean>>
 
@@ -45,17 +46,59 @@ const LogoutButton = ({ setLoggedIn } : { setLoggedIn: booleanSetState}) => {
 	)
 }
 
-const requestUserData = () => {
+const requestUserData = async ({ setUserData } : { setUserData: React.Dispatch<React.SetStateAction<{[key: string]: any}>>}) => {
 	const tokenData: tokenData = jwt(document.cookie);
-	axios.get('http://localhost:3000/users/' + tokenData.id).then((response) => {})
+	await axios.get('http://localhost:3000/users/' + tokenData.id).then((response) => {setUserData({
+		id: response.data.id,
+		username: response.data.username,
+		rating: response.data.rating,
+		email: response.data.email,
+		status: response.data.status,
+		image_url: response.data.image_url,
+		external_id: response.data.external_id
+})})
 }
 
-const ProfileInfo = () => {
+const ProfileInfo = (data: {[key: string]: any}) => {
+	let statusColor: string
+	switch( data.userData.status ) {
+		case 'online':
+			statusColor = '#4CAF50';
+			break ;
+		case 'offline':
+			statusColor = '#1E1E1E';
+			break;
+		case 'away':
+			statusColor = '#F39810';
+			break;
+		case 'in_game':
+			statusColor = '#9575CD';
+			break;
+		default: statusColor = '#FF0000';
+	}
 	return(
 		<>
-		<Typography sx={{ color: '#1E1E1E', fontFamily: 'Orbitron', fontWeight: 600, fontSize: '4vh' }}>
-			username
-		</Typography>
+		<Stack display='flex' flexDirection='row' alignItems="center" sx={{ paddingTop: '0.5vh', paddingLeft: '1.5vh', paddingBottom: '2vh' }}>
+			<Box component='img' src={data.userData.image_url} alt='Profile picture'
+				sx={{
+					height: '5vh',
+					width: '5vh',
+					borderRadius: 2,
+					boxShadow: 1
+				}}>
+			</Box>
+				<Typography sx={{ color: '#1E1E1E', fontFamily: 'Orbitron', fontWeight: 600, fontSize: '4vh', paddingLeft: '1.7vh', paddingRight: '1.7vh'}}>
+					{data.userData.username}
+				</Typography>
+			<Box
+			sx={{ 
+				backgroundColor: statusColor,
+				height: '3vh',
+				width: '3vh',
+				borderRadius: 50,
+				boxShadow: 1}}>
+		</Box>
+		</Stack>
 		</>
 	)
 }
@@ -66,7 +109,7 @@ const Background = () => {
 					sx={{
 						backgroundColor: '#311B92'
 					}}>
-					<Typography sx={{ fontSize: '7em', fontFamily: 'Orbitron', fontWeight: 500, color: '#FFFFFF', textShadow: '0px 0px 6px #FFFFFF'}}>
+					<Typography sx={{ fontSize: '14vh', fontFamily: 'Orbitron', fontWeight: 500, color: '#FFFFFF', textShadow: '0px 0px 6px #FFFFFF'}}>
 						ft_transcendence
 					</Typography>
 		</Box>
@@ -74,10 +117,14 @@ const Background = () => {
 }
 
 export const Home = ({ setLoggedIn } : { setLoggedIn: booleanSetState}) => {
+
+	const [userData, setUserData] = useState<{[key: string]: any}>({});
+
+	useEffect(() => {requestUserData({setUserData})}, []);	
 		return (
 			<>
 				<AppBar sx={{ height: '7vh', background: '#F5F5F5'}}>
-					<ProfileInfo/>
+					<ProfileInfo userData={userData}/>
 				</AppBar>
 				<Background />
 				<Box display="flex" justifyContent="right" minHeight="10vh"
