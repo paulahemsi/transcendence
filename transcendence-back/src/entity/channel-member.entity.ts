4d1133a6477c1ac22c9f5f5a5ab495efbf5c7750
paulahemsi <paulahemsi@gmail.com>
2022-09-12 15:37:02 -0300

    feat(chat): create channel member entity

---
+++ transcendence-back/src/entity/channel-member.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Channel } from './channel.entity';
+import { User } from './user.entity';
+
+@Entity()
+export class ChannelMember {
+  @PrimaryGeneratedColumn({
+    type: 'bigint',
+  })
+  id: number;
+
+  @ManyToOne(() => Channel, (channel) => channel.id, {
+    nullable: false,
+  })
+  channel: Channel;
+
+  @ManyToOne(() => User, (user) => user.id, {
+    nullable: false,
+  })
+  user: User;
+}
--- transcendence-back/src/entity/index.ts
+++ transcendence-back/src/entity/index.ts
@@ -4,8 +4,9 @@ import { Friendship } from './friendship.entity';
 import { Channel } from './channel.entity';
 import { Message } from './message.entity';
 import { ChannelType } from './channel-type.entity';
+import { ChannelMember } from './channel-member.entity';
 
-const entities = [User, MatchHistory, Friendship, Channel, Message, ChannelType];
+const entities = [User, MatchHistory, Friendship, Channel, Message, ChannelType, ChannelMember];
 
-export { User, MatchHistory, Friendship, Channel, Message, ChannelType };
+export { User, MatchHistory, Friendship, Channel, Message, ChannelType, ChannelMember };
 export default entities;
