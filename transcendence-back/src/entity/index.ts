ba9035dd6b35dab5fafdca808fa803df41e5c0c8
paulahemsi <paulahemsi@gmail.com>
2022-09-12 15:42:43 -0300

    feat(chat): create channel admin entity

--- transcendence-back/src/entity/index.ts
+++ transcendence-back/src/entity/index.ts
@@ -5,8 +5,9 @@ import { Channel } from './channel.entity';
 import { Message } from './message.entity';
 import { ChannelType } from './channel-type.entity';
 import { ChannelMember } from './channel-member.entity';
+import { ChannelAdmin } from './channel-admin.entity';
 
-const entities = [User, MatchHistory, Friendship, Channel, Message, ChannelType, ChannelMember];
+const entities = [User, MatchHistory, Friendship, Channel, Message, ChannelType, ChannelMember, ChannelAdmin];
 
-export { User, MatchHistory, Friendship, Channel, Message, ChannelType, ChannelMember };
+export { User, MatchHistory, Friendship, Channel, Message, ChannelType, ChannelMember, ChannelAdmin };
 export default entities;
