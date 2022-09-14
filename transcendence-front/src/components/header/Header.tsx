8bb53ec95337a1c3fccbd8f48aa0e77391c451fa
paulahemsi <paulahemsi@gmail.com>
2022-09-14 18:05:37 -0300

    refactor(ProfileInfo): unify components at same ProfileInfo file

--- transcendence-front/src/components/header/Header.tsx
+++ transcendence-front/src/components/header/Header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { AppBar } from "@mui/material"
+import { AppBar, Stack } from "@mui/material"
 import ProfileInfo from "./ProfileInfo";
 
 export const Header = ({ userData } : {[key: string]: any}) => {
 	return (
-		<AppBar sx={{ height: '7vh', background: '#F5F5F5'}}>
+	<AppBar sx={{ height: '7vh', background: '#F5F5F5'}}>
+		<Stack display='flex' flexDirection='row' alignItems="center" sx={{ paddingTop: '0.5vh', paddingLeft: '1.5vh', paddingBottom: '2vh' }}>
 			<ProfileInfo userData={userData}/>
-		</AppBar>
+		</Stack>
+	</AppBar>
 	)
 }
 
--- transcendence-front/src/components/header/ProfileInfo.tsx
+++ transcendence-front/src/components/header/ProfileInfo.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import { Stack } from "@mui/material"
-import UserStatus from "./UserStatus";
-import UserImage from "./UserImage";
-import UserName from "./UserName";
+import { Box, Typography } from "@mui/material"
 
 const defineColor = (status: string) => {
 	switch( status ) {
@@ -18,14 +15,46 @@ const defineColor = (status: string) => {
 	}
 }
 
+const UserImage = ({imageUrl} : {imageUrl: string}) => {
+	return (
+		<Box component='img' src={imageUrl} alt='Profile picture'
+				sx={{
+					height: '5vh',
+					width: '5vh',
+					borderRadius: 2,
+					boxShadow: 1
+				}}>
+		</Box>
+	)
+}
+
+const UserName = ({userName} : {userName : string}) => {
+	return (
+		<Typography sx={{ color: '#1E1E1E', fontFamily: 'Orbitron', fontWeight: 600, fontSize: '4vh', paddingLeft: '1.7vh', paddingRight: '1.7vh'}}>
+					{userName}
+		</Typography>
+	)
+}
+
+const UserStatus = ({statusColor} : {statusColor : string}) => {
+	return (
+		<Box
+			sx={{ 
+				backgroundColor: statusColor,
+				height: '3vh',
+				width: '3vh',
+				borderRadius: 50,
+				boxShadow: 1}}>
+		</Box>
+	)
+}
+
 export const ProfileInfo = ({ userData } : {[key: string]: any}) => {
 	return(
 		<>
-		<Stack display='flex' flexDirection='row' alignItems="center" sx={{ paddingTop: '0.5vh', paddingLeft: '1.5vh', paddingBottom: '2vh' }}>
 			<UserImage imageUrl={userData.image_url}/>
 			<UserName userName={userData.username}/>
 			<UserStatus statusColor={defineColor(userData.status)}/>
-		</Stack>
 		</>
 	)
 }
