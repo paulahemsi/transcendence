c43ae81fd746be40a2c20b6ac15e59f0ebcbee94
paulahemsi <paulahemsi@gmail.com>
2022-09-24 13:48:02 -0300

    feat(channel): add update channel service structure

--- transcendence-back/src/channels/channels.service.ts
+++ transcendence-back/src/channels/channels.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ChannelDto } from 'src/dto/channel.dtos';
 import { Channel } from 'src/entity';
 import { Repository } from 'typeorm';
 
@@ -9,4 +10,8 @@ export class ChannelsService {
     @InjectRepository(Channel)
     private readonly channelRepository: Repository<Channel>,
   ) {}
+  
+  async update(id: number, channelDto: ChannelDto) {
+    console.log("update channel", id)
+  }
 }
