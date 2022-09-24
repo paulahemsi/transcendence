d2cbfa991e18b37dc2dec1084aaf2b424a2dd0da
paulahemsi <paulahemsi@gmail.com>
2022-09-24 13:47:38 -0300

    feat(channel): add update channel endpoint

--- transcendence-back/src/channels/channels.controller.ts
+++ transcendence-back/src/channels/channels.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Param, Patch } from '@nestjs/common';
+import { ChannelDto } from 'src/dto/channel.dtos';
 import { ChannelsService } from './channels.service';
 
 @Controller('channels')
 export class ChannelsController {
   constructor(private readonly channelService: ChannelsService) {}
+  
+  @Patch(':id')
+  @HttpCode(204)
+  updateUser(
+    @Param('id') id: number,
+    @Body() channelDto: ChannelDto,
+  ) {
+    return this.channelService.update(id, channelDto);
+  }
 }
