acaa1ffcd12c64df558864da48ce307c46daea59
Lais Arena <laisarena@gmail.com>
2022-10-02 16:49:19 -0300

    feat(channel-type): initialize channel type table

--- transcendence-back/src/channels/channel-type.service.ts
+++ transcendence-back/src/channels/channel-type.service.ts
@@ -8,7 +8,21 @@ export class ChannelTypeService {
   constructor(
     @InjectRepository(ChannelType)
     private readonly channelTypeRepository: Repository<ChannelType>,
-  ) {}
+  ) {
+    this.addChannelType(channelType.PUBLIC);
+    this.addChannelType(channelType.PRIVATE);
+    this.addChannelType(channelType.PROTECTED);
+  }
+
+  async addChannelType(type: channelType) {
+    if (await this.channelTypeRepository.findOneBy({ type })) {
+      return;
+    }
+    const channelType = this.channelTypeRepository.create({
+      type: type,
+    });
+    this.channelTypeRepository.save(channelType);
+  }
 
   async getChannelType(channelType: channelType) {
     return this.channelTypeRepository.findOneBy({ type: channelType });
