cb05861f72923170d967e23fc078d394dc68b6e1
paulahemsi <paulahemsi@gmail.com>
2022-10-12 13:20:35 -0300

    refactor(chat): create FriendsList component

--- transcendence-front/src/components/chatDrawer/ChatDrawer.tsx
+++ transcendence-front/src/components/chatDrawer/ChatDrawer.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, useState } from "react";
-import { Box, Button, Drawer, List, ListItem } from '@mui/material';
-import FriendsInfo from "./FriendsInfo";
+import { Drawer, List } from '@mui/material';
 import ChatButton from "./ChatButton";
+import FriendsList from "./FriendsList";
 
 type booleanSetState = React.Dispatch<React.SetStateAction<boolean>>
 
@@ -10,38 +10,18 @@ interface Props {
     setOpenDrawer: booleanSetState;
 }
 
-const onHoverFriend = {
-	'&:hover': {
-		backgroundColor: '#B998FF',
-	},
-	width: '100%',
-	textTransform: 'lowercase',
-	borderRadius: '0'
-}
-
 export const ChatDrawer : FunctionComponent<Props> = ({ friendsData, setOpenDrawer }) => {
 	const [direct, setDirect] = useState(true);
-	
-	const friends = [] as JSX.Element[];
-	friendsData.forEach((element : {[key: string]: any}) => {
-		friends.push(
-		<ListItem disablePadding key={element.username} sx={{marginBottom: '1vh'}}> 
-			<Button sx={onHoverFriend} onClick={() => console.log("click")}>
-				<FriendsInfo userData={element}/>
-			</Button>
-		</ListItem>);
-	})
+
 	return (
 		<>
 		  <Drawer open={true} transitionDuration={500} onClose={() => setOpenDrawer(false)} anchor="right">
 			<ChatButton direct={direct} setDirect={setDirect}/>
 			{
 				direct
-				? <List disablePadding>
-						{friends}
-				</List>
+				? <FriendsList friendsData={friendsData} />
 				: <List disablePadding>
-					{friends}
+					blablabla
 				</List>
 			}
 		  </Drawer>
