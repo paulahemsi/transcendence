90055c336026eb80742d330c8fae43eae6813988
lrcouto <laurarccouto@gmail.com>
2022-10-17 12:41:13 -0300

    Add collision test

--- transcendence-front/src/components/game/game.tsx
+++ transcendence-front/src/components/game/game.tsx
@@ -27,31 +27,59 @@ export class PhaserGame extends React.Component {
 		new Phaser.Game(gameConfig);
 
 		let player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+		let ball: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 		let cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+		let ballVelocity : number[] = [1000, -1000];
 
 		function preload(this: Phaser.Scene): void {
 	
 			this.load.image('pad', require('./assets/pad.png'));
+			this.load.image('ball', require('./assets/ball.png'));
 		}
 
 		function create(this: Phaser.Scene): void {
 			player = this.physics.add.sprite(100, 450, 'pad');
+			ball = this.physics.add.sprite(this.sys.canvas.height / 2, this.sys.canvas.height / 2, 'ball');
+
 			player.setCollideWorldBounds(true);
+			player.body.setImmovable(true);
+			ball.setCollideWorldBounds(true);
+			ball.setAcceleration(0);
+			this.time.delayedCall(1000, start, [], this);
 		}
 
 		function update(this: Phaser.Scene): void {
 			cursors = this.input.keyboard.createCursorKeys();
 			if (cursors.up.isDown) {
-				player.setVelocityY(-300);
+				player.setVelocityY(-500);
 			}
 			else if (cursors.down.isDown) {
-				player.setVelocityY(300);
+				player.setVelocityY(500);
 			}
 			else {
 				player.setVelocityY(0);
 			}
 		}
+
+	function HandleCollision(this: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody) : void {
+		if (this.y < ball.y)
+		{
+		ball.setVelocityY(-10 * (this.y - ball.y));
+		}
+		else if (this.y > ball.y)
+		{
+		ball.setVelocityY(10 * (ball.y - this.y));
+		}
+	}	
+
+	function start(this: Phaser.Scene) : void {
+		(ball.body as Phaser.Physics.Arcade.Body).onWorldBounds = true;
+		ball.body.velocity.setTo(ballVelocity[Math.floor(Math.random() * 2)], ballVelocity[Math.floor(Math.random() * 2)]);
+		ball.setBounce(1);
+		this.physics.add.collider(ball, player, HandleCollision, undefined, player);
 	}
+}
+
 	shouldComponentUpdate(): boolean {
 		return false;
 	}
