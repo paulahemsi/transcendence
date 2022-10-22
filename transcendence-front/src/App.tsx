68940a471351498ff8e2b1cb2a28d822b6ca95eb
Lais Arena <laisarena@gmail.com>
2022-10-21 21:09:34 -0300

    fix: deal with expired token

--- transcendence-front/src/App.tsx
+++ transcendence-front/src/App.tsx
@@ -3,22 +3,40 @@ import './App.css';
 import { useState } from 'react';
 import LoginCard from './components/LoginCard'
 import Home from './components/Home'
+import jwt from 'jwt-decode';
 
-  function isLoggedIn() {
-    let cookie = document.cookie;
-    return (cookie.includes("accessToken") ? true : false);
-  }
+type tokenData = {
+	id: string;
+	exp: number;
+}
 
-  function App() {
-    const[ loggedIn, setLoggedIn ] = useState(isLoggedIn());
-    return (
-    <>
-      <main>
-          {loggedIn
-          ? <Home setLoggedIn={setLoggedIn}/>
-          : <LoginCard/> }
-      </main>
-    </>
+function validateToken(cookie: string) {
+  const currentDate = new Date();
+	const tokenData: tokenData = jwt(cookie);
+	if (tokenData.exp * 1000 < currentDate.getTime()) {
+		return false;
+	}
+	return true;
+
+}
+
+function isLoggedIn() {
+  const cookie = document.cookie;
+  if (!cookie.includes("accessToken"))
+    return false;
+  return validateToken(cookie);
+}
+
+function App() {
+  const[ loggedIn, setLoggedIn ] = useState(isLoggedIn());
+  return (
+  <>
+    <main>
+        {loggedIn
+        ? <Home setLoggedIn={setLoggedIn}/>
+        : <LoginCard/> }
+    </main>
+  </>
   );
 }
 
