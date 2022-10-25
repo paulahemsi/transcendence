e39afca4ef309995429f65bec7f709474f346204
paulahemsi <paulahemsi@gmail.com>
2022-10-24 21:58:07 -0300

    feat(joinGroup): implement ask for  password flow

--- transcendence-front/src/components/chatDrawer/ControlPanel/AddGroupsDialog.tsx
+++ transcendence-front/src/components/chatDrawer/ControlPanel/AddGroupsDialog.tsx
@@ -1,16 +1,25 @@
 import React, { useState, FunctionComponent, useEffect } from "react"
-import { Button, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material"
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material"
 import axios, { AxiosRequestHeaders } from 'axios';
 import jwt from 'jwt-decode';
-import UsersList from "./UsersList";
 import SearchGroupsList from "./SearchGroupsList";
 
 type booleanSetState = React.Dispatch<React.SetStateAction<boolean>>
 type objectSetState = React.Dispatch<React.SetStateAction<{[key: string]: any}>>
 
+const PROTECTED = 'PROTECTED';
+const PUBLIC = 'PUBLIC'
+const PASSWORD_TITLE = 'O.O Uuuh... It seems this is a protected group...'
+const JOIN_TITLE = 'Join Group'
+const JOIN_LABEL = 'Group name'
+const PASSWORD_LABEL = 'Password'
+const JOIN_BUTTON = 'Add'
+const PASSWORD_BUTTON = 'Join'
+
 type groupsData = {
 	name: string;
 	id: string;
+	type: string;
 }
 
 type tokenData = {
@@ -26,18 +35,23 @@ export const AddGroupsDialog : FunctionComponent<Props> = ({ setOpenDialog, setG
 	const [groupsList, setGroupsList] = useState<{[key: string]: any}>({});
 	const [loading, setLoading] = useState<boolean>(true);
 	const [searchQuery, setSearchQuery] = useState("");
+	const [title, setTitle] = useState(JOIN_TITLE);
+	const [label, setLabel] = useState(JOIN_LABEL);
+	const [button, setButton] = useState(JOIN_BUTTON);
+
 	const tokenData: tokenData = jwt(document.cookie);
 	const authToken: AxiosRequestHeaders = {'Authorization': 'Bearer ' + document.cookie.substring('accessToken='.length)};
 
+	let groupName = "";
+	let password = "";
+	
+	
 	const handleQuery = (event :  React.ChangeEvent<HTMLInputElement>) => {
 		setSearchQuery(event.target.value);
 	}
 
-	const keyDownHandler = ( event :  React.KeyboardEvent<HTMLInputElement>) => {
-		if (event.key === 'Enter') {
-			event.preventDefault();
-			handleSave();
-		}
+	const selectedGroup = () => {
+		return groupsList.filter((group: {[key: string]: any}) => group.name === groupName)
 	}
 
 	const requestGroupsData = async () => {
@@ -47,6 +61,7 @@ export const AddGroupsDialog : FunctionComponent<Props> = ({ setOpenDialog, setG
 					const newGroup : groupsData = {
 						name: groupData.name,
 						id: groupData.id,
+						type: groupData.type
 					}
 					groupsList.push(newGroup);
 			});
@@ -62,7 +77,7 @@ export const AddGroupsDialog : FunctionComponent<Props> = ({ setOpenDialog, setG
 	}
 	
 	const handleSave = () => {
-		const group = groupsList.filter((group: {[key: string]: any}) => group.name === searchQuery);
+		const group = selectedGroup();
 		axios.post(`http://localhost:3000/channels/${group[0].id}/members`, {
 			"userId": tokenData.id
 		}, { headers: authToken }).then( () => {
@@ -71,12 +86,37 @@ export const AddGroupsDialog : FunctionComponent<Props> = ({ setOpenDialog, setG
 		})
 	}
 	
+	const handleJoin = () => {
+		password = searchQuery;
+		handleSave();
+	}
+	
+	const handleAdd = () => {
+		groupName = searchQuery;
+		const group = selectedGroup();
+		if ( group[0].type === PUBLIC ) {
+			handleSave();
+		} else if ( group[0].type === PROTECTED ) {
+			setTitle(PASSWORD_TITLE);
+			setLabel(PASSWORD_LABEL);
+			setButton(PASSWORD_BUTTON);
+			setSearchQuery("");
+		}
+	}
+
+	const keyDownHandler = ( event :  React.KeyboardEvent<HTMLInputElement>) => {
+		if (event.key === 'Enter') {
+			event.preventDefault();
+			(title === JOIN_TITLE) ? handleAdd() : handleJoin();
+		}
+	}
+
 	useEffect(() => {requestGroupsData()}, []);
 
 	return (
 		<>
 		<DialogTitle sx={{fontFamily: 'Orbitron'}}>
-			Join Groups
+			{title}
 		</DialogTitle>
 		<DialogContent>
 			<TextField
@@ -86,13 +126,14 @@ export const AddGroupsDialog : FunctionComponent<Props> = ({ setOpenDialog, setG
 				type="email"
 				fullWidth
 				variant="standard"
+				label={label}
 				value={searchQuery}
 				onKeyDown={keyDownHandler}
 				onChange={handleQuery}
 			/>
 		</DialogContent>
 		{
-			!loading && 
+			!loading && ( title === JOIN_TITLE ) &&
 			<SearchGroupsList groupsList={groupsList} searchQuery={searchQuery} />
 		}
 		<DialogActions>
@@ -104,10 +145,10 @@ export const AddGroupsDialog : FunctionComponent<Props> = ({ setOpenDialog, setG
 		</Button>
 		<Button
 			variant="contained"
-			onClick={handleSave}
+			onClick={(title === JOIN_TITLE) ? handleAdd : handleJoin}
 			sx={{fontFamily: 'Orbitron'}}
 		>
-			Add
+			{button}
 		</Button>
 		</DialogActions>
 	</>
