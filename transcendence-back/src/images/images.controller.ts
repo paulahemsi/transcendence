3b72b5876221332dfadf0de4733302b8e9b1c324
Lais Arena <laisarena@gmail.com>
2022-10-30 12:08:37 -0300

    feat(upload): implement post image

--- transcendence-back/src/images/images.controller.ts
+++ transcendence-back/src/images/images.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller('images')
-export class ImagesController {}
+export class ImagesController {
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename(_, file, callback) {
+          return callback(null, `${file.originalname}`);
+        },
+      }),
+    }),
+  )
+  uploadImage(@UploadedFile() file: any) {
+    console.log(file.filename);
+  }
+}
