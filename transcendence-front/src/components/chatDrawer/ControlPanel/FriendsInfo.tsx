d968896ad9054dc1243b9850dfd04411523b01b1
Lais Arena <laisarena@gmail.com>
2022-11-20 20:05:18 -0300

    refactor: move status colors to constants

--- transcendence-front/src/components/chatDrawer/ControlPanel/FriendsInfo.tsx
+++ transcendence-front/src/components/chatDrawer/ControlPanel/FriendsInfo.tsx
@@ -1,19 +1,6 @@
 import React from "react";
 import { Box, Typography } from "@mui/material"
-
-const defineColor = (status: string) => {
-	switch( status ) {
-		case 'online':
-			return '#4CAF50';
-		case 'offline':
-			return '#1E1E1E';
-		case 'away':
-			return '#F39810';
-		case 'in_game':
-			return '#9575CD';
-		default: return '#FF0000';
-	}
-}
+import { defineColor } from "../../utils/constants";
 
 const UserImage = ({imageUrl} : {imageUrl: string}) => {
 	return (
--- transcendence-front/src/components/profileDrawer/main-infos/MainInfos.tsx
+++ transcendence-front/src/components/profileDrawer/main-infos/MainInfos.tsx
@@ -4,20 +4,7 @@ import { UserStatus } from "./UserStatus";
 import { UserImage } from "./UserImage";
 import { UserName } from "./UserName";
 import { WinsAndLosses } from "./WinsAndLosses";
-
-const defineColor = (status: string) => {
-	switch( status ) {
-		case 'online':
-			return '#4CAF50';
-		case 'offline':
-			return '#1E1E1E';
-		case 'away':
-			return '#F39810';
-		case 'in_game':
-			return '#9575CD';
-		default: return '#FF0000';
-	}
-}
+import { defineColor } from "../../utils/constants";
 
 export const MainInfos = ({userProfile} : {userProfile: {[key: string]: any}}) => {
 	return (
