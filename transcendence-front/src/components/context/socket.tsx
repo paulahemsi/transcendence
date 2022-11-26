798c9b922604e2eea8235a7e69e6305363e5cd17
Lais Arena <laisarena@gmail.com>
2022-11-26 14:43:49 -0300

    fix: complete socket url

--- transcendence-front/src/components/context/socket.tsx
+++ transcendence-front/src/components/context/socket.tsx
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-export const chatSocket = io('/chat');
-export const sessionSocket = io('/session');
-export const gameSocket = io('/game');
+const backUrl = 'http://localhost:4444'
+export const chatSocket = io(`${backUrl}/chat`);
+export const sessionSocket = io(`${backUrl}/session`);
+export const gameSocket = io(`${backUrl}/game`);
 
