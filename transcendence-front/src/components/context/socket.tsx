8137594d7c657493dbc0b83e4c41f73fd2a7162b
Lais Arena <laisarena@gmail.com>
2022-11-28 17:09:09 -0300

    fix(socket): add cookie to socket conection

--- transcendence-front/src/components/context/socket.tsx
+++ transcendence-front/src/components/context/socket.tsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const backUrl = 'http://localhost:4444'
-export const chatSocket = io(`${backUrl}/chat`);
-export const sessionSocket = io(`${backUrl}/session`);
-export const gameSocket = io(`${backUrl}/game`);
+export const chatSocket = io(`${backUrl}/chat`, { withCredentials: true });
+export const sessionSocket = io(`${backUrl}/session`, { withCredentials: true });
+export const gameSocket = io(`${backUrl}/game`, { withCredentials: true });
 
