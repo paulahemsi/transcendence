5f5a98b79bb2de224d92e75a431439878c466270
lrcouto <laurarccouto@gmail.com>
2023-06-10 13:46:49 -0300

    Attempt to configure SSL for DB

--- transcendence-back/src/database/database.module.ts
+++ transcendence-back/src/database/database.module.ts
@@ -14,6 +14,7 @@ import entities from '../entity';
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
+        ssl: configService.get('DB_SSL'),
         entities: entities,
         synchronize: true,
       }),
